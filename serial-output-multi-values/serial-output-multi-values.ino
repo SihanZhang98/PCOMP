8a5318e2e53c582aed3430a76bc19b1512a00cd3
SihanZhang98 <70332584+SihanZhang98@users.noreply.github.com>
2020-10-21 19:23:14 +0800

    Update serial-output-multi-values.ino

--- serial-output-multi-values/serial-output-multi-values.ino
+++ serial-output-multi-values/serial-output-multi-values.ino
@@ -1,17 +1,16 @@
 
 void setup() {
-  Serial.begin(9600);    // initialize serial communications
-  Serial.setTimeout(10); // set the timeout for parseInt
+  Serial.begin(9600);    
+  Serial.setTimeout(10); 
 }
  
 void loop() {
-  if (Serial.available() > 0) { // if there's serial data available
+  if (Serial.available() > 0) { 
     int inDataX = Serial.parseInt(); 
     int inDataY = Serial.parseInt();
     // read it
-    if (Serial.read() == '\n') {
-//      Serial.write(inByte);      // send it back out as raw binary data
-      analogWrite(5, inDataX);    // use it to set the LED brightness 
+    if (Serial.read() == '\n') {     
+      analogWrite(5, inDataX);    
       analogWrite(6, inDataY);
     }
   }
